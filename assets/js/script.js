bdc54013c41a8cd610b5f70564e063922f216bd9
00shivani <shivanisingh13@icloud.com>
2023-12-13 15:39:00 -0500

    final changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,46 @@
-
 $(document).ready(function () {
 
-  // Variables
-  var currentDate =  dayjs().format('dddd, MMM DD YYYY');
-  var currentHour = dayjs().format("HH");
-  var currentHourInt = parseInt(currentHour);
-  var saveButton = $(".saveBtn");
-  var timeBlock = $(".time-block");
+  // DOM Elements
+  var $currentDateElement = $("#currentDay");
+  var $saveButtons = $(".saveBtn");
+  var $timeBlocks = $(".time-block");
 
-  // Print current date to header:
-  $("#currentDay").html(currentDate);
+  // Current Time Variables
+  var currentDateString = dayjs().format('dddd, MMM DD YYYY');
+  var currentHour = parseInt(dayjs().format("HH"));
 
-  // "Save" button click listener
-  saveButton.on("click", function () {
-    // collecting user input and correlating hour value
-    var input = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    // saving time and user input values in local storage
-    localStorage.setItem(time, input);
-  })
+  // Display current date in the header
+  $currentDateElement.text(currentDateString);
 
-  // Function to switch timeblock colors based on the time of day
-  function timeColor() {
+  // Event listener for "Save" button click
+  $saveButtons.on("click", function () {
+    var $userInput = $(this).siblings(".description").val();
+    var blockTimeId = $(this).parent().attr("id");
+    localStorage.setItem(blockTimeId, $userInput);
+  });
 
-    timeBlock.each(function () {
-      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-      
-      if (blockTime < currentHourInt) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
+  // Function to update time block colors based on the current time
+  function updateTimeBlockColors() {
+    $timeBlocks.each(function () {
+      var blockTimeId = $(this).attr("id");
+      var blockHour = parseInt(blockTimeId.split("hour")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass("future present").addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past future").addClass("present");
+      } else {
+        $(this).removeClass("past present").addClass("future");
       }
-      else if (blockTime === currentHourInt) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-      }
-      else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-      }
-    })
+    });
   }
   
-   // Getting user inputs from local storage
-   $("#hour9 .description").val(localStorage.getItem("hour9"));
-   $("#hour10 .description").val(localStorage.getItem("hour10"));
-   $("#hour11 .description").val(localStorage.getItem("hour11"));
-   $("#hour12 .description").val(localStorage.getItem("hour12"));
-   $("#hour13 .description").val(localStorage.getItem("hour13"));
-   $("#hour14 .description").val(localStorage.getItem("hour14"));
-   $("#hour15 .description").val(localStorage.getItem("hour15"));
-   $("#hour16 .description").val(localStorage.getItem("hour16"));
-   $("#hour17 .description").val(localStorage.getItem("hour17"));
-  
-   // Call function
-   timeColor();
-  
+  // Retrieve user inputs from local storage
+  for (var hour = 9; hour <= 17; hour++) {
+    var blockTimeId = "hour" + hour;
+    $("#" + blockTimeId + " .description").val(localStorage.getItem(blockTimeId));
+  }
+
+  // Call the function to update time block colors
+  updateTimeBlockColors();
 });
